ea1f03f48ad4d28d04de0248a8106b97451af00d
SeagullOddy <shabbyacc@outlook.com>
2023-11-16 20:38:19 +0800

    Some unimportant changes

--- src/main/java/com/fruit/controller/BaseController.java
+++ src/main/java/com/fruit/controller/BaseController.java
@@ -1,17 +1,19 @@
 package com.fruit.controller;
-import org.springframework.web.bind.WebDataBinder;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
 
-import java.text.*;
-import java.util.Date;
-
 public abstract class BaseController {
-    @InitBinder
-    public void initBinder(WebDataBinder binder) {
-        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        dateFormat.setLenient(true);
-        binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, true));
-    }
+
+  @InitBinder
+  public void initBinder(WebDataBinder binder) {
+    DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+    dateFormat.setLenient(true);
+    binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, true));
+  }
+
 }
